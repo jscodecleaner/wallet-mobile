a258be7b6f60037956deb55e3ae38480cdd14bbb
webdevsmart <webdevsmart@hotmail.com>
2022-05-10 01:28:56 +0900

    fix: padding issue

--- src/screens/Dashboard/WalletDetails/WalletDetails.style.ts
+++ src/screens/Dashboard/WalletDetails/WalletDetails.style.ts
@@ -3,7 +3,6 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
     container: {
       width: '100%',
-      flex: 1,
       paddingHorizontal: 15,
       paddingVertical: 20
     },
--- src/screens/Dashboard/WalletList/WalletList.style.ts
+++ src/screens/Dashboard/WalletList/WalletList.style.ts
@@ -3,7 +3,6 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
     container: {
       width: '100%',
-      flex: 1,
       alignItems: 'center',
       paddingHorizontal: 15,
       paddingTop: 20
