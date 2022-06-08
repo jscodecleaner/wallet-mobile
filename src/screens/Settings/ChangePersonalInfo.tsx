465a277f317747b8931fd3a2d1e8f9c7f9b14dc9
flynn31 <minato20010119@gmail.com>
2022-06-08 21:27:08 +0900

    qqq

--- src/screens/Settings/ChangePersonalInfo.tsx
+++ src/screens/Settings/ChangePersonalInfo.tsx
@@ -1,9 +1,89 @@
-import React, { useState } from 'react';
-import { TextInput, Button, Text, HelperText, withTheme } from 'react-native-paper';
+import React, { useState, useMemo } from 'react';
+import { View, Image, StatusBar, SafeAreaView } from 'react-native';
+import { TextInput, Button, Text, HelperText, withTheme, RadioButton } from 'react-native-paper';
+import countryList from 'react-select-country-list'
+import SelectDropdown from 'react-native-select-dropdown';
+import FontAwesomeIcons from 'react-native-vector-icons/FontAwesome';
+import CountryFlag from "react-native-country-flag";
+
+import { useStyles } from './Settings.style'
 
 const ChangePersonalInfo = ({ theme, navigation }) => {
+
+  const styles = useStyles(theme)
+
+  const [checked, setChecked] = useState('male')
+
+  const listOfCountry = useMemo(() => countryList().getData(), []);
+  const [recipientCountry, setRecipientCountry] = useState('');
+
   return (
-    <Text>personalInfo</Text>
+    <View>
+      <View>
+
+        <View style={styles.row} onTouchStart={() => setChecked('male')}>
+          <RadioButton
+            value="Male"
+            color='black'
+            status={checked === 'male' ? 'checked' : 'unchecked'}
+            onPress={() => setChecked('male')}
+          />
+          <Text style={{ padding: 7 }}>Male</Text>
+        </View>
+        <View style={styles.row} onTouchStart={() => setChecked('female')}>
+          <RadioButton
+            value="Female"
+            color='black'
+            status={checked === 'female' ? 'checked' : 'unchecked'}
+            onPress={() => setChecked('female')}
+          />
+          <Text style={{ padding: 7 }}>Female</Text>
+        </View>
+        <View style={styles.row} onTouchStart={() => setChecked('unknown')}>
+          <RadioButton
+            value="Unknown"
+            color='black'
+            status={checked === 'unknown' ? 'checked' : 'unchecked'}
+          />
+          <Text style={{ padding: 7 }}>Unknown</Text>
+        </View>
+
+      </View>
+
+      <View>
+        {/* <TextInput autoComplete="birthdate-full"></TextInput> */}
+      </View>
+
+      <View>
+        <SelectDropdown
+          data={listOfCountry}
+          onSelect={(selectedItem, index) => {
+            setRecipientCountry(selectedItem.value)
+          }}
+          buttonStyle={styles.countryDropdownBtnStyle}
+          renderCustomizedButtonChild={(selectedItem, index) => {
+            return (
+              <View style={styles.countryDropdownBtnChildStyle}>
+                <Text style={styles.countryDropdownBtnTxt}>{selectedItem ? selectedItem.label : 'Country'}</Text>
+                <FontAwesomeIcons name="chevron-down" color={theme.colors.text} size={14} />
+              </View>
+            )
+          }}
+          dropdownOverlayColor="transparent"
+          dropdownStyle={styles.countryDropdownDropdownStyle}
+          rowStyle={styles.countryDropdownRowStyle}
+          renderCustomizedRowChild={(item, index) => {
+            return (
+              <View style={styles.countryDropdownRowChildStyle}>
+                <CountryFlag isoCode={item.value} size={25} />
+                <Text style={styles.countryDropdownRowTxt}>{item.label}</Text>
+              </View>
+            );
+          }}
+        />
+      </View>
+
+    </View>
   );
 };
 
--- src/screens/Settings/Settings.style.ts
+++ src/screens/Settings/Settings.style.ts
@@ -1,35 +1,74 @@
 import { StyleSheet } from 'react-native';
 
 export const useStyles = theme => (StyleSheet.create({
-    container: {
-      width: '100%',
-    },
-    transactionContainer: {
-      padding: 20
-    },
-    row: {
-      flexDirection: 'row',
-      marginBottom: 10
-    },
-    leftText: {
-      width: '40%',
-      fontSize: 18,
-      marginRight: 10
-    },
-    rightText: {
-      width: '60%',
-      fontSize: 18,
-    },
-    actionsContainer: {
-      marginTop: 30,
-      paddingHorizontal: 20,
-      flexDirection: 'row',
-    },
-    input: {
-      marginVertical: 10,
-      height: 50,
-      backgroundColor: "transparent",
-      borderBottomWidth: 1,
-      borderColor: theme.colors.text,
-    },
+  container: {
+    width: '100%',
+  },
+  transactionContainer: {
+    padding: 20
+  },
+  row: {
+    flexDirection: 'row',
+    marginBottom: 10
+  },
+  leftText: {
+    width: '40%',
+    fontSize: 18,
+    marginRight: 10
+  },
+  rightText: {
+    width: '60%',
+    fontSize: 18,
+  },
+  actionsContainer: {
+    marginTop: 30,
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+  },
+  input: {
+    marginVertical: 10,
+    height: 50,
+    backgroundColor: "transparent",
+    borderBottomWidth: 1,
+    borderColor: theme.colors.text,
+  },
+  countryDropdownBtnStyle: {
+    width: '100%',
+    height: 50,
+    paddingHorizontal: 0,
+    borderBottomWidth: 1,
+    borderColor: theme.colors.text,
+  },
+  countryDropdownBtnChildStyle: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingRight: 18,
+  },
+  countryDropdownBtnTxt: {
+    color: theme.colors.text,
+    textAlign: 'center',
+    fontSize: 16,
+    marginHorizontal: 12,
+  },
+  countryDropdownDropdownStyle: { backgroundColor: '#fff' },
+  countryDropdownRowStyle: {
+    backgroundColor: '#fff',
+    borderBottomColor: theme.colors.text,
+    height: 50,
+  },
+  countryDropdownRowChildStyle: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingHorizontal: 18,
+  },
+  countryDropdownRowTxt: {
+    color: theme.colors.text,
+    textAlign: 'center',
+    fontSize: 16,
+    marginHorizontal: 12,
+  },
 }));
