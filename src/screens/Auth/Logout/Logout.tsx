4bc1cd29ae13a36168787908ce3cbaf7b18658f9
webdevsmart <webdevsmart@hotmail.com>
2022-08-31 23:06:22 +0900

    fix: logout function

--- src/screens/Auth/Logout/Logout.tsx
+++ src/screens/Auth/Logout/Logout.tsx
@@ -2,18 +2,41 @@ import React, { useState } from 'react';
 import { View, Image, SafeAreaView, ScrollView } from 'react-native'; 
 import { TextInput, Button, Text, withTheme } from 'react-native-paper';
 import Spinner from 'react-native-loading-spinner-overlay';
-import styles from './Logout.style';
 import { useDispatch } from 'react-redux';
+
+import styles from './Logout.style';
 import CustomButton from '../../../components/CustomButton/CustomButton';
+import { BASE_URL, getProxyUrl } from '../../../services/common';
+import { ApiEndpoint, StatusCode } from '../../../types/enum';
+import { universalPostRequestWithData } from '../../../services/RequestHandler';
+import { Logout } from '../../../redux/slices/userSlice';
 
 const LogoutScreen = (({theme, navigation}) => {
+	const dispatch = useDispatch();
+
+	const [progress, setProgress] = useState(false);
 
 	const onClickCancel = () => {
 		navigation.navigate('WalletList');
 	}
 
-	const onClickLogout = () => {
+	const onClickLogout = async () => {
 		navigation.navigate('Login');
+		if (progress === true) {
+      return;
+    }
+    setProgress(true);
+
+    const url = `${BASE_URL}/${ApiEndpoint.LOGOUT}`;
+    const data = {
+      'white-label': getProxyUrl(),
+    };
+
+    const response: any = await universalPostRequestWithData(url, data);
+		dispatch(Logout());
+		navigation.navigate('Login');
+
+    setProgress(false);
 	}
 
 	return (
